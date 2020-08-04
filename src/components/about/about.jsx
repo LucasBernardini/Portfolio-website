cc20e905edf22f1c47fb1a646887bf1f06bbecec
Lucas Bernardini <lucas.bernardini97@gmail.com>
2020-08-04 08:40:00 -0400

    Remodel the about section of website

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,35 +1,35 @@
 import React from "react";
-import lucas from '../../assets/lucas-animation.png';
+import lucas from "../../assets/lucas-animation.png";
 import "./about.scss";
 
 export default function About() {
   return (
     <div className="about">
-        <div className="about__container about__container--1">
-      <h2 className="about__title" >About Me</h2>
-      <img className="about__image" src={lucas} alt="animated profile"/>
+      <div className="about__container about__container--1">
+        <h2 className="about__title">About Me</h2>
+        <img className="about__image" src={lucas} alt="animated profile" />
       </div>
       <div className="about__container">
-      <p className="about__text">
-        I originally come from a background in banking and finance. I attended
-        York University and achieved a Bachelors of Commerce degree, then I went
-        to work for Bank of Montreal in various roles. I was never satisfied
-        with the work I was doing and felt very limited. Once realizing this, I
-        then began to look for industries where I am able to constantly learn,
-        have more creative freedom and implement technologies that are
-        revolutionizing society's future. This is when I stumbled upon web
-        development. I have completed BrainStations 12 week web development
-        bootcamp where I learned to use and implement various technologies such
-        as HTML, CSS/SASS, Javascript - React, Node, and MySQL.
-        <br />
-        <br />
-        Im am currently available for hire and/or employment opportunities. If
-        you would like to get in contact with me please check out the contact
-        section of my website.
-        <br />
-        <br />
-        Thanks for stopping by!
-      </p>
+        <p className="about__text">
+          I have a passion for developing websites and web applications using
+          Javascript. The majority of my projects are built using React.js in
+          conjunction with CSS/SASS. I am always learning and building on my
+          existing tools, whether that is advanced knowledge of my current
+          skills or new technologies. Becoming a developer has always been a
+          goal of mine due to my passions for technology, creativity and problem
+          solving. I have spent the beginning of my working life in the banking
+          and finance industry. Pivoting to development and tech has been an
+          amazing journey and I am looking forward to the future of the web
+          development industry!
+          <br />
+          <br />
+          Im am currently available for hire and/or employment opportunities. If
+          you would like to get in contact with me please check out the contact
+          section of my website.
+          <br />
+          <br />
+          Thanks for stopping by!
+        </p>
       </div>
     </div>
   );
